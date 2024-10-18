25cdbb1b596ad2ebd4c657c1bc3b9bb4b023c45a
Casey Blamires <clblamires@gmail.com>
2024-10-18 14:38:40 -0700

    Update Schedule.tsx

--- src/pages/Schedule.tsx
+++ src/pages/Schedule.tsx
@@ -1,6 +1,11 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { useMasterData } from '../hooks/useMasterData';
 
 const Schedule: React.FC = () => {
+    const { data, loading, error } = useMasterData();
+    const enrollments = data?.enrollments;
+    const schedule = enrollments?.filter( (course:any) => course.student_id == "0123456789" )
+
 
     const pageTitle = "Schedule";
 
